c0e12c07db895b6067b735a8c1a827303b256a67
Alexander Vyzhymov <vizimov@gmail.com>
2023-03-21 09:23:27 +0200

    fixed-span

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -3,11 +3,14 @@ import css from './FriendListItem.module.css';
 const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
     <li className={css.item}>
-      {isOnline ? (
-        <span className={`${css.status} ${css.isOnline}`}></span>
-      ) : (
-        <span className={`${css.status} ${css.isOffline}`}></span>
-      )}
+      <span
+        className={
+          isOnline
+            ? `${css.status} ${css.isOnline}`
+            : `${css.status} ${css.isOffline}`
+        }
+      ></span>
+
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
