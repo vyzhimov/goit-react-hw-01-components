e60831a7f0ad51dc4c7bc0fbf6e9e968da39fad3
Alexander Vyzhymov <vizimov@gmail.com>
2023-03-21 22:00:35 +0200

    fixed-errors

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -4,13 +4,8 @@ const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
     <li className={css.item}>
       <span
-        className={
-          isOnline
-            ? `${css.status} ${css.isOnline}`
-            : `${css.status} ${css.isOffline}`
-        }
+        className={`${css.status} ${isOnline ? css.isOnline : css.isOffline}`}
       ></span>
-
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
